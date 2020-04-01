cca66060f07c058d5c9a07e2b0fcb2d71dcbce60
InterferenceObject <admin@ot-sdt-serv.com>
2020-04-01 21:46:15 +0200

    WIP

--- boost.config.js
+++ boost.config.js
@@ -1,24 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
-import _project from '../../boost.config'
 
-function extend(obj, src){
-    for (var key in src) {
-        if (src.hasOwnProperty(key)) obj[key] = src[key];
-    }
-    return obj;
-}
-
-function getProjectModulesForGroup(){
-    let mod_groups = [];
-    for(var key in _project.modules){
-        mod_groups.push(key);
-    }
-    return mod_groups;
-}
-
-export default {
+module.exports = {
     version: '1.0.0',
-    api: _project.api,
     jwt: {
         secret: ''
     },
@@ -26,64 +9,51 @@ export default {
         location: 'https://www.sanctum.epicdev.co.za',
         project_key: ''
     },
-    modules: extend(
-        {
-            'admin': {
-                title: "Dashboard",
-                icon: "mdi-view-dashboard-outline",
-                to_prefix: '',
-                tag: '',
-                tag_color: colors.blue.base
-            },
-            'users': {
-                title: "Users",
-                icon: "mdi-account-outline",
-                to_prefix: '/admin',
-                tag: 'in dev',
-                tag_color: colors.red.base
-            },
-            'roles': {
-                title: "Roles",
-                icon: "mdi-clipboard-outline",
-                to_prefix: '/admin',
-                tag: '',
-                tag_color: colors.blue.base
-            },
-            'debug-logger': {
-                title: "Debug Logger",
-                icon: "mdi-radar",
-                to_prefix: '/admin',
-                tag: '',
-                tag_color: colors.blue.base
-            },
-            'preferences': {
-                title: "Preferences",
-                icon: "mdi-settings-outline",
-                to_prefix: '/admin',
-                tag: '',
-                tag_color: colors.blue.base
-            }
+    modules: {
+        'admin': {
+            title: "Dashboard",
+            icon: "mdi-view-dashboard-outline",
+            to_prefix: '',
+            tag: '',
+            tag_color: colors.blue.base
         },
-        _project.modules
-    ),
-    module_groups: function () {
-        let mod_groups = [
-            {
-                title: null,
-                modules: [
-                    'admin'
-                ]
-            }
-        ];
-
-        if(getProjectModulesForGroup().length > 0){
-            mod_groups.push({
-                title: "Your Modules",
-                modules: getProjectModulesForGroup()
-            });
+        'users': {
+            title: "Users",
+            icon: "mdi-account-outline",
+            to_prefix: '/admin',
+            tag: 'in dev',
+            tag_color: colors.red.base
+        },
+        'roles': {
+            title: "Roles",
+            icon: "mdi-clipboard-outline",
+            to_prefix: '/admin',
+            tag: '',
+            tag_color: colors.blue.base
+        },
+        'debug-logger': {
+            title: "Debug Logger",
+            icon: "mdi-radar",
+            to_prefix: '/admin',
+            tag: '',
+            tag_color: colors.blue.base
+        },
+        'preferences': {
+            title: "Preferences",
+            icon: "mdi-settings-outline",
+            to_prefix: '/admin',
+            tag: '',
+            tag_color: colors.blue.base
         }
-
-        mod_groups.push({
+    },
+    module_groups: [
+        {
+            title: null,
+            modules: [
+                'admin'
+            ]
+        },
+        {
             title: "System",
             modules: [
                 'users',
@@ -91,8 +61,6 @@ export default {
                 'debug-logger',
                 'preferences'
             ]
-        });
-
-        return mod_groups;
-    }()
-}
+        }
+    ]
+};

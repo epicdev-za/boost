58a77554b4a33619598021360bb20f9125ed2bc3
InterferenceObject <admin@ot-sdt-serv.com>
2020-04-29 14:18:10 +0200

    Fixed logo on login page

--- plugins/boost-plugin.js
+++ plugins/boost-plugin.js
@@ -2,16 +2,8 @@ import Vue from 'vue';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-import LogoIcon from './../components/LogoIcon.vue';
-
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    icons: {
-        values: {
-            logo: {
-                component: LogoIcon
-            }
-        }
-    }
+
 });

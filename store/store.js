6865ffcb01f38db6af2a65675fcfedf39dd88b52
InterferenceObject <admin@ot-sdt-serv.com>
2020-09-22 12:51:00 +0200

    Store WIP

--- store/store.js
+++ store/store.js
@@ -1,11 +1,3 @@
-const fs = require("fs");
-
 export default (() => {
-	let project_store = {};
-
-	if(fs.existsSync("../../../store/store.js")){
-    	project_store = require("../../../store/store");
-	}
-
-    return project_store;
+    return require("../../../store/store");
 })();

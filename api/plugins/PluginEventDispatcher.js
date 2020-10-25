2932c052aa6b05b219ba0fbe40bd9bae97dcbc4d
InterferenceObject <admin@ot-sdt-serv.com>
2020-10-25 18:02:02 +0200

    WIP

--- api/plugins/PluginEventDispatcher.js
+++ api/plugins/PluginEventDispatcher.js
@@ -19,6 +19,7 @@ class PluginEventDispatcher {
     }
 
     static onSanctumEncrypt(data, project_key){
+    	console.log(this.plugins);
         for(let i = 0; i < this.plugins.length; i++){
             let res = this.plugins[i].onSanctumEncrypt(data, project_key);
             if(res !== undefined && res !== null){

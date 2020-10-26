76e325f9b94c59fc70f1aa0f2006bc627881b98d
InterferenceObject <admin@ot-sdt-serv.com>
2020-10-26 14:05:16 +0200

    WIP

--- api/SanctumUtil.js
+++ api/SanctumUtil.js
@@ -24,6 +24,8 @@ class SanctumUtil {
                 };
 
                 client.post(endpoint + "?project=" + project_key, packet, (err, req, res, obj) => {
+                	console.log(err);
+                	console.log(obj);
                     if(err){
                         if(typeof cres === typeof undefined || cres === null){
                             reject(err);
